831b08c484e025ed0173f7e9b65571e32eb90034
Ignitz <moritz.pertl@gmx.at>
2024-03-09 13:13:00 +0100

    liking works

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.movieappmad24
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,7 +17,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Home
@@ -30,9 +29,7 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -41,25 +38,68 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
 import coil.compose.AsyncImage
-import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
 
 class MainActivity : ComponentActivity() {
     var likedList = mutableListOf<Movie>()
 
+
+    @OptIn(ExperimentalMaterial3Api::class)
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppMAD24Theme {
                 // A surface container using the 'background' color from the theme
-                page()
+                val movieList = remember {
+                    mutableStateOf(getMovies())
+                }
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = { Text(text = "mMovies") })
+                    },
+                    bottomBar = {
+                        BottomAppBar({
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceAround
+                            ) {
+                                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                    IconButton(
+                                        onClick = { movieList.value = getMovies() }) {
+                                        Icon(
+                                            imageVector = Icons.Default.Home,
+                                            contentDescription = "Back"
+                                        )
+                                    }
+                                    Text(
+                                        text = "Home",
+                                        fontSize = 12.sp
+                                    )
+                                }
+                                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                    IconButton(onClick = { movieList.value = likedList }) {
+                                        Icon(
+                                            imageVector = Icons.Default.Star,
+                                            contentDescription = "Back"
+                                        )
+                                    }
+                                    Text(
+                                        text = "Favourites",
+                                        fontSize = 12.sp
+                                    )
+                                }
+                            }
+                        })
+                    }) { paddingValues ->
+                    movieList(list = movieList.value, paddingValues = paddingValues)
+                }
             }
         }
     }
@@ -68,52 +108,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     @OptIn(ExperimentalMaterial3Api::class)
     fun page() {
-        val movieList = remember {
-            mutableStateOf(getMovies())
-        }
-        Scaffold(
-            topBar = {
-                TopAppBar(title = { Text(text = "mMovies") })
-            },
-            bottomBar = {
-                BottomAppBar({
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceAround
-                    ) {
-                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
-                            IconButton(
-                                onClick = { movieList.value = getMovies() }) {
-                                Icon(
-                                    imageVector = Icons.Default.Home,
-                                    contentDescription = "Back"
-                                )
-                            }
-                            Text(
-                                text = "Home",
-                                fontSize = 12.sp
-                            )
-                        }
-                        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                            IconButton(onClick = { movieList.value = likedList }) {
-                                Icon(
-                                    imageVector = Icons.Default.Star,
-                                    contentDescription = "Back"
-                                )
-                            }
-                            Text(
-                                //modifier = Modifier.fillMaxWidth(.33f),
-                                text = "Favourites",
-                                fontSize = 12.sp
-                            )
-                        }
-                    }
-                })
-            }) { paddingValues ->
-            movieList(list = movieList.value, paddingValues = paddingValues)
-        }
     }
 
     @Composable
@@ -135,35 +130,45 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MovieCard(movie: Movie) {
-        val arrowIcon = remember { mutableStateOf(Icons.Default.KeyboardArrowDown) }
-        val liked = remember { mutableStateOf(if (movie !in likedList) Icons.Default.FavoriteBorder else Icons.Default.Favorite) }
-        Log.d("liking", (movie in likedList).toString())
+        val arrow = remember { mutableStateOf(false) }
+        val liked =
+            remember { mutableStateOf(movie in likedList) }
+        val imageUrl = remember { mutableStateOf(movie.images[0]) }
+        var imageIndex = 0
+        liked.value = movie in likedList
+        imageUrl.value = movie.images[imageIndex]
         Card(
             modifier = Modifier.padding(all = 5.dp),
         ) {
             Box {
                 AsyncImage(
-                    model = movie.images[0],
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(imageUrl.value)
+                        .crossfade(true)
+                        .build()
+                    ,
                     contentDescription = null,
+                    modifier = Modifier.clickable(onClick = {
+                        imageIndex = (imageIndex+1) % movie.images.size
+                        imageUrl.value = movie.images[imageIndex]
+                    }),
                 )
                 IconButton(onClick = {
-                    if (liked.value == Icons.Default.FavoriteBorder && movie !in likedList) {
-                        liked.value = Icons.Default.Favorite
-                        if (movie !in likedList) {
-                            likedList.add(movie)
-                        }
-                        Log.d("likedList", likedList.toString())
-                    } else {
-                        liked.value = Icons.Default.FavoriteBorder
+                    if (liked.value) {
+                        liked.value = false
                         if (movie in likedList) {
                             likedList.remove(movie)
                         }
-                        Log.d("likedList", likedList.toString())
+                    } else {
+                        liked.value = true
+                        if (movie !in likedList) {
+                            likedList.add(movie)
+                        }
                     }
                 }, modifier = Modifier.align(Alignment.TopEnd)) {
                     Icon(
-                        imageVector = liked.value,
-                        tint = if (liked.value == Icons.Default.FavoriteBorder) Color.LightGray else Color.Red,
+                        imageVector = if (liked.value) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        tint = if (liked.value) Color.Red else Color.LightGray,
                         contentDescription = "Back"
                     )
                 }
@@ -179,17 +184,15 @@ class MainActivity : ComponentActivity() {
                     fontSize = 20.sp
                 )
                 IconButton(onClick = {
-                    if (arrowIcon.value == Icons.Default.KeyboardArrowDown) arrowIcon.value =
-                        Icons.Default.KeyboardArrowUp else arrowIcon.value =
-                        Icons.Default.KeyboardArrowDown
+                    arrow.value = !arrow.value
                 }) {
                     Icon(
-                        imageVector = arrowIcon.value,
+                        imageVector = if (arrow.value) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
                         contentDescription = "Back"
                     )
                 }
             }
-            if (arrowIcon.value == Icons.Default.KeyboardArrowUp) {
+            AnimatedVisibility(arrow.value) {
                 Column(modifier = Modifier.padding(all = 10.dp)) {
                     Text(text = "Director: " + movie.director)
                     Text(text = "Released: " + movie.year)
